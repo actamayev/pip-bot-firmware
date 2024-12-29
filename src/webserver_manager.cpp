752adb2f16884d4929bc1ab6d7f8d1a8a069d8be
atamayev <75034340+actamayev@users.noreply.github.com>
2024-12-29 13:20:35 -0500

    Cleanup

--- src/webserver_manager.cpp
+++ src/webserver_manager.cpp
@@ -74,47 +74,6 @@ void WebServerManager::startWebServer() {
 		}
 	});
 
-	// server.on("/", []() {
-	// 	String html = "<html><body><h1>Wi-Fi Configuration</h1>"
-	// 					"<form action=\"/connect\" method=\"POST\">"
-	// 					"SSID: <input type=\"text\" name=\"ssid\"><br>"
-	// 					"Password: <input type=\"text\" name=\"password\"><br>"
-	// 					"<input type=\"submit\" value=\"Connect\">"
-	// 					"</form></body></html>";
-	// 	server.send(200, "text/html", html);
-	// });
-
-	// server.on("/connect", []() {
-	// 	String ssid = server.arg("ssid");
-	// 	String password = server.arg("password");
-
-	// 	bool connectionStatus = WiFiManager::getInstance().attemptNewWifiConnection({ ssid, password });
-
-	// 	if (connectionStatus) {
-	// 		String successHtml =
-	// 			"<html><body><h1>Connected successfully!</h1>"
-	// 			"<p>Please close this tab and re-connect your computer to your previously connected wifi</p></body></html>";
-
-	// 		preferences.begin("wifi-creds", false);
-	// 		preferences.putString("ssid", ssid);
-	// 		preferences.putString("password", password);
-	// 		preferences.end();
-	// 		server.send(200, "text/html", successHtml);
-	// 		Serial.println("Wi-Fi connected.");
-	// 	} else {
-	// 		String errorHtml = "<html><body><h1>Failed to connect to Wi-Fi</h1>"
-	// 							"<p>Please try again.</p></body></html>";
-	// 		server.send(500, "text/html", errorHtml);
-			
-	// 		// Clear the entered Wifi ssid/password
-	// 		preferences.begin("wifi-creds", false);  
-	// 		preferences.clear();  // Clear all WiFi credentials from "wifi-creds" namespace
-	// 		preferences.end();    // Close the preferences to free up resources
-
-	// 		Serial.println("Failed to connect to Wi-Fi. No reboot.");
-	// 	}
-	// });
-
 	server.onNotFound([]() {
 		server.send(404, "text/plain", "Not Found");
 	});
