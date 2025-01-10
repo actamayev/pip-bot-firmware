a7a17096d8ad5110fc5879bc2ec34daae67210f9
atamayev <75034340+actamayev@users.noreply.github.com>
2025-01-10 11:12:01 -0500

    Update user_code.cpp

--- src/user_code.cpp
+++ src/user_code.cpp
@@ -5,15 +5,4 @@
 #include "./include/user_code.h"
 
 void user_code() {
-    float pitchAngle = Sensors::getInstance().getPitch();
-
-    if (pitchAngle >= 0 && pitchAngle <= 10) {
-        rgbLed.set_led_blue();  // Level position
-    } else if (pitchAngle > 10) {
-        rgbLed.set_led_red();   // Tilted up
-    } else {
-        rgbLed.set_led_green(); // Tilted down
-    }
-
-    delay(50);
 }
