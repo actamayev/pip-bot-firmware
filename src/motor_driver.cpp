35c590c153201cbaa3b2830e57d0e2ba0e11ade2
atamayev <75034340+actamayev@users.noreply.github.com>
2025-02-24 16:47:16 -0500

    Add encoder methods

--- src/motor_driver.cpp
+++ src/motor_driver.cpp
@@ -17,8 +17,8 @@ MotorDriver::MotorDriver() {
     pinMode(RIGHT_MOTOR_ENCODER_B, INPUT_PULLUP);
 
     // Attach interrupts for encoders (assuming ENC1_A and ENC2_A are defined in config.h)
-    // attachInterrupt(LEFT_MOTOR_ENCODER_A, encoder1_isr, RISING);
-    // attachInterrupt(RIGHT_MOTOR_ENCODER_A, encoder2_isr, RISING);
+    attachInterrupt(LEFT_MOTOR_ENCODER_A, encoder1_isr, RISING);
+    attachInterrupt(RIGHT_MOTOR_ENCODER_A, encoder2_isr, RISING);
 }
 
 void MotorDriver::both_motors_forward(uint8_t speed) {
@@ -80,50 +80,10 @@ void MotorDriver::right_motor_stop() {
     analogWrite(RIGHT_MOTOR_PIN_IN_2, 0);
 }
 
-// void IRAM_ATTR MotorDriver::encoder1_isr() {
-//     motorDriver.leftEncoderCount++;  // Increment left encoder count
-// }
+void IRAM_ATTR MotorDriver::encoder1_isr() {
+    motorDriver.leftEncoderCount++;  // Increment left encoder count
+}
 
-// void IRAM_ATTR MotorDriver::encoder2_isr() {
-//     motorDriver.rightEncoderCount++;  // Increment right encoder count
-// }
-
-// void MotorDriver::setTargetDistance(int16_t leftSpeed, int16_t rightSpeed) {
-//     leftSpeed = constrain(leftSpeed, -255, 255);
-//     rightSpeed = constrain(rightSpeed, -255, 255);
-
-//     if (leftSpeed == 0 && rightSpeed == 0) {
-//         leftTargetCount = leftEncoderCount;  // Stop by setting target to current position
-//         rightTargetCount = rightEncoderCount;
-//         leftCurrentSpeed = 0;
-//         rightCurrentSpeed = 0;
-//     } else {
-//         // For each command, add/subtract TICKS_PER_COMMAND based on direction
-//         leftCurrentSpeed = abs(leftSpeed);
-//         rightCurrentSpeed = abs(rightSpeed);
-//         int leftDirection = (leftSpeed > 0) ? 1 : (leftSpeed < 0) ? -1 : 0;
-//         int rightDirection = (rightSpeed > 0) ? 1 : (rightSpeed < 0) ? -1 : 0;
-//         leftTargetCount += leftDirection * TICKS_PER_COMMAND;
-//         rightTargetCount += rightDirection * TICKS_PER_COMMAND;
-//     }
-// }
-
-// void MotorDriver::updateMotorControl() {
-//     // Left motor control
-//     if (leftEncoderCount < leftTargetCount) {
-//         left_motor_forward(leftCurrentSpeed);
-//     } else if (leftEncoderCount > leftTargetCount) {
-//         left_motor_backward(leftCurrentSpeed);
-//     } else {
-//         left_motor_stop();
-//     }
-
-//     // Right motor control
-//     if (rightEncoderCount < rightTargetCount) {
-//         right_motor_forward(rightCurrentSpeed);
-//     } else if (rightEncoderCount > rightTargetCount) {
-//         right_motor_backward(rightCurrentSpeed);
-//     } else {
-//         right_motor_stop();
-//     }
-// }
+void IRAM_ATTR MotorDriver::encoder2_isr() {
+    motorDriver.rightEncoderCount++;  // Increment right encoder count
+}
