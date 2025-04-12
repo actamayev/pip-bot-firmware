b316cd8faee63eaeb0b1434fc5899ee00965d732
Ariel Tamayev <actamayev@gmail.com>
2025-04-12 13:55:50 -0400

    Turn of SLD for backward drive (#52) (#53)
    * Cleanup
    
    * Correct LEDs
    
    * Cleanup
    
    * Simplify motor driver
    
    * Remove SLD for backwards driving
    
    * Update platformio.ini
    
    * Update straight_line_drive.cpp

--- src/actuators/motor_driver.h
+++ src/actuators/motor_driver.h
@@ -7,10 +7,6 @@
 class MotorDriver {
     public:
         MotorDriver();  // Constructor to initialize pins
-        void both_motors_forward(uint8_t speed = 255);
-        void both_motors_backward(uint8_t speed = 255);
-        void rotate_clockwise(uint8_t speed = 255);
-        void rotate_counterclockwise(uint8_t speed = 255);
         void stop_both_motors();
         void left_motor_forward(uint8_t speed = 255);
         void left_motor_backward(uint8_t speed = 255);
