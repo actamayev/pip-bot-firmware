10a52a59c7ae6ec3baf30e4641a17b5971fed7fb
atamayev <bluedotrobots@gmail.com>
2025-04-21 12:42:15 -0400

    Set firmware version before restart

--- src/networking/firmware_version_tracker.h
+++ src/networking/firmware_version_tracker.h
@@ -13,11 +13,8 @@ class FirmwareVersionTracker : public Singleton<FirmwareVersionTracker> {
 
     public:
         int getFirmwareVersion() { return firmwareVersion; }
-        void setFirmwareVersion(int version);
-        void setPendingVersion(int version);
         void retrieveLatestFirmwareFromServer(uint16_t newVersion);
-        bool isUpdating() { return isRetrievingFirmwareFromServer; }
-        
+
     private:
         FirmwareVersionTracker();
         int firmwareVersion = 0;
