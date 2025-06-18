342d896e8c37b39917fce20822346e7000962584
atamayev <bluedotrobots@gmail.com>
2025-06-18 15:38:30 -0400

    Change timeout length

--- src/utils/timeout_manager.h
+++ src/utils/timeout_manager.h
@@ -9,18 +9,18 @@ class TimeoutManager : public Singleton<TimeoutManager> {
         void resetActivity();
         void update();
         bool isInConfirmationState() const { return inConfirmationState; }
-        void cancelConfirmation();
-
-    private:
+        
+        private:
         TimeoutManager() = default;
         
         unsigned long lastActivityTime = 0;
         bool inConfirmationState = false;
         unsigned long confirmationStartTime = 0;
-        
-        static constexpr unsigned long INACTIVITY_TIMEOUT = 10 * 1000; // 15 seconds
-        static constexpr unsigned long CONFIRMATION_TIMEOUT = 10 * 1000; // 1 seconds
-        
+
+        static constexpr unsigned long INACTIVITY_TIMEOUT = 9 * 60 * 1000; // 9 minutes
+        static constexpr unsigned long CONFIRMATION_TIMEOUT = 1 * 60 * 1000; // 1 minute
+
+        void cancelConfirmation();
         void enterConfirmationState();
         void enterDeepSleep();
 };
