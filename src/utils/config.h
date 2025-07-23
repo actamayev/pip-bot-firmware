8a6c14d3c31311a31fad6dd7a1748bee6c7047d6
atamayev <bluedotrobots@gmail.com>
2025-07-23 10:18:48 -0400

    Update config.h

--- src/utils/config.h
+++ src/utils/config.h
@@ -102,7 +102,7 @@ inline const char* getEnvironment() {
 inline const char* getServerFirmwareEndpoint() {
     std::string env = getEnvironment();
     if (env == "local") {
-        return "http://10.232.146.40:8080/pip/firmware-update";
+        return "http://10.132.109.40:8080/pip/firmware-update";
     } else if (env == "staging") {
         return "https://staging-api.bluedotrobots.com/pip/firmware-update";
     }
@@ -112,7 +112,7 @@ inline const char* getServerFirmwareEndpoint() {
 inline const char* getWsServerUrl() {
     std::string env = getEnvironment();
     if (env == "local") {
-        return "ws://10.232.146.40:8080/esp32";
+        return "ws://10.132.109.40:8080/esp32";
     } else if (env == "staging") {
         return "wss://staging-api.bluedotrobots.com/esp32";
     }
